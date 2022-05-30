282fe7b45dd286018d6e0328cd6ae58ce4ca07e7
alwaystaegi <ffff4565@sunmoon.ac.kr>
2022-05-30 09:02:29 +0900

    파이어베이스 관련 업데이트 중...

--- app/src/main/java/osp/smgonggu/myapplication/JoinActivity.java
+++ app/src/main/java/osp/smgonggu/myapplication/JoinActivity.java
@@ -12,6 +12,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
@@ -19,10 +24,13 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class JoinActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;// 파이어베이스 연동할 데이터베이스 리퍼런스 함수
 
     // 로그 찍을 때 사용하는 TAG 변수
     final private String TAG = getClass().getSimpleName();
@@ -40,100 +48,49 @@ public class JoinActivity extends AppCompatActivity {
         userid_et = findViewById(R.id.userid_et);
         passwd_et = findViewById(R.id.passwd_et);
         join_button = findViewById(R.id.join_button);
+        mDatabase = FirebaseDatabase.getInstance().getReference(); //
 
 // 버튼 이벤트 추가
         join_button.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
 // 회원가입 함수 호출
-                JoinTask joinTask = new JoinTask();
-                joinTask.execute(userid_et.getText().toString(), passwd_et.getText().toString());
+                String getUserId = userid_et.getText().toString();
+                String getUserPassword = passwd_et.getText().toString();
+
+                //hashmap 만들기
+                //hashmap?: 해시코드를 생성해서 해시맵에 있는 요소(이 코드로 치면
+                HashMap result = new HashMap<>();
+                result.put("Id", getUserId);
+                result.put("password", getUserPassword);
+
+                writeNewUser("1", getUserId, getUserPassword);
+
             }
         });
+
     }
+    private void writeNewUser(String userId, String Id, String password) {
+        User user = new User(Id, password);
 
-    class JoinTask extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-            Log.d(TAG, "onPreExecute");
-        }
-
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-
-
-            Log.d(TAG, "onPostExecute, " + result);
-
-// 결과값이 success 로 나오면
-            if(result.equals("success")){
-
-//토스트 메시지를 뿌리고, 이전 액티비티(LoginActivity) 로 돌아감
-                Toast.makeText(getApplicationContext(), "성공적으로 회원가입 되었습니다.", Toast.LENGTH_SHORT).show();
-                finish();
-            }else
-            {
-                Toast.makeText(JoinActivity.this, result, Toast.LENGTH_SHORT).show();
-            }
-        }
-
-
-        @Override
-        protected String doInBackground(String... params) {
-
-            String userid = params[0];
-            String passwd = params[1];
-
-            String server_url = "http://15.164.252.136/join.php";
-
-
-            URL url;
-            String response = "";
-            try {
-                url = new URL(server_url);
-
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(15000);
-                conn.setConnectTimeout(15000);
-                conn.setRequestMethod("POST");
-                conn.setDoInput(true);
-                conn.setDoOutput(true);
-                Uri.Builder builder = new Uri.Builder()
-                        .appendQueryParameter("userid", userid)
-                        .appendQueryParameter("passwd", passwd);
-                String query = builder.build().getEncodedQuery();
-
-                OutputStream os = conn.getOutputStream();
-                BufferedWriter writer = new BufferedWriter(
-                        new OutputStreamWriter(os, "UTF-8"));
-                writer.write(query);
-                writer.flush();
-                writer.close();
-                os.close();
-
-                conn.connect();
-                int responseCode=conn.getResponseCode();
-
-                if (responseCode == HttpsURLConnection.HTTP_OK) {
-                    String line;
-                    BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                    while ((line=br.readLine()) != null) {
-                        response+=line;
+        mDatabase.child("users").child(userId).setValue(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Write was successful!
+                        Toast.makeText(MainActivity.this, "저장을 완료했습니다.", Toast.LENGTH_SHORT).show();
                     }
-                }
-                else {
-                    response="";
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Write failed
+                        Toast.makeText(MainActivity.this, "저장을 실패했습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                });
 
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            return response;
-        }
     }
+
+
 }
--- app/src/main/java/osp/smgonggu/myapplication/LoginActivity.java
+++ app/src/main/java/osp/smgonggu/myapplication/LoginActivity.java
@@ -1,5 +1,6 @@
 package osp.smgonggu.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,6 +12,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -24,8 +43,16 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private DatabaseReference mDatabase;// 파이어베이스 연동할 데이터베이스 리퍼런스 함수
+
     // 로그에 사용할 TAG 변수 선언
     final private String TAG = getClass().getSimpleName();
+
+    //구글로그인을 하기 위한 함수
+    private FirebaseAuth mAuth = null;
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 9001;
+    private SignInButton signInButton;
 
     // 사용할 컴포넌트 선언
     EditText userid_et, passwd_et;
@@ -36,6 +63,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mDatabase = FirebaseDatabase.getInstance().getReference(); //
 // 사용할 컴포넌트 초기화
         userid_et = findViewById(R.id.userid_et);
         passwd_et = findViewById(R.id.passwd_et);
@@ -92,7 +120,47 @@ public class LoginActivity extends AppCompatActivity {
             }
         }
 
+        private void writeNewUser(String userId, String name ,String password) {
 
+            User user = new User(name);
+
+            mDatabase.child("users").child(userId).setValue(user)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            // Write was successful!
+                            Toast.makeText(LoginActivity.this, "저장을 완료했습니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            // Write failed
+                            Toast.makeText(LoginActivity.this, "저장을 실패했습니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+        }
+        private void readUser(){
+            mDatabase.child("users").child("1").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    // Get Post object and use the values to update the UI
+                    if(dataSnapshot.getValue(User.class) != null){
+                        User post = dataSnapshot.getValue(User.class);
+                        Log.w("FireBaseData", "getData" + post.toString());
+                    } else {
+                        Toast.makeText(LoginActivity.this, "데이터 없음...", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    // Getting Post failed, log a message
+                    Log.w("FireBaseData", "loadPost:onCancelled", databaseError.toException());
+                }
+            });
+        }
         @Override
         protected String doInBackground(String... params) {
 
