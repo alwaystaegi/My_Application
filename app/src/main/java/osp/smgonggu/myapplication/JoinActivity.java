d72727aee0b6a192239fc35fbf0e12e0448dfbed
alwaystaegi <ffff4565@sunmoon.ac.kr>
2022-06-01 20:53:00 +0900

    회원가입&로그인까지 구현

--- app/src/main/java/osp/smgonggu/myapplication/JoinActivity.java
+++ app/src/main/java/osp/smgonggu/myapplication/JoinActivity.java
@@ -1,10 +1,10 @@
 package osp.smgonggu.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,10 +12,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.*;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -24,13 +27,13 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class JoinActivity extends AppCompatActivity {
-
-    private DatabaseReference mDatabase;// 파이어베이스 연동할 데이터베이스 리퍼런스 함수
 
     // 로그 찍을 때 사용하는 TAG 변수
     final private String TAG = getClass().getSimpleName();
@@ -38,6 +41,9 @@ public class JoinActivity extends AppCompatActivity {
     // 사용할 컴포넌트 선언
     EditText userid_et, passwd_et;
     Button join_button;
+
+    //firebase에 연결할 함수
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +54,9 @@ public class JoinActivity extends AppCompatActivity {
         userid_et = findViewById(R.id.userid_et);
         passwd_et = findViewById(R.id.passwd_et);
         join_button = findViewById(R.id.join_button);
-        mDatabase = FirebaseDatabase.getInstance().getReference(); //
+
+        //firebase의 인스턴스 초기화
+        mAuth = FirebaseAuth.getInstance();
 
 // 버튼 이벤트 추가
         join_button.setOnClickListener(new View.OnClickListener() {
@@ -58,39 +66,57 @@ public class JoinActivity extends AppCompatActivity {
 // 회원가입 함수 호출
                 String getUserId = userid_et.getText().toString();
                 String getUserPassword = passwd_et.getText().toString();
-
-                //hashmap 만들기
-                //hashmap?: 해시코드를 생성해서 해시맵에 있는 요소(이 코드로 치면
-                HashMap result = new HashMap<>();
-                result.put("Id", getUserId);
-                result.put("password", getUserPassword);
-
-                writeNewUser("1", getUserId, getUserPassword);
+                Toast.makeText(JoinActivity.this, getUserId,
+                        Toast.LENGTH_SHORT).show();
+                createAccount(getUserId,getUserPassword);
 
             }
-        });
+            });
 
-    }
-    private void writeNewUser(String userId, String Id, String password) {
-        User user = new User(Id, password);
+        }
 
-        mDatabase.child("users").child(userId).setValue(user)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
+
+    private void createAccount(String email, String password) {
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
-                    public void onSuccess(Void aVoid) {
-                        // Write was successful!
-                        Toast.makeText(MainActivity.this, "저장을 완료했습니다.", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        // Write failed
-                        Toast.makeText(MainActivity.this, "저장을 실패했습니다.", Toast.LENGTH_SHORT).show();
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            String email = user.getEmail();
+                            String uid = user.getUid();
+
+                            //해쉬맵 테이블을 파이어베이스 데이터베이스에 저장
+                            HashMap<Object,String> hashMap = new HashMap<>();
+
+                            hashMap.put("uid",uid);
+                            hashMap.put("email",email);
+
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            DatabaseReference reference = database.getReference("Users");
+                            reference.child(uid).setValue(hashMap);
+
+
+                            //가입이 이루어져을시 가입 화면을 빠져나감.
+                            Intent intent = new Intent(JoinActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                            finish();
+                            Toast.makeText(JoinActivity.this, "회원가입에 성공하셨습니다.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                        //    Toast.makeText(JoinActivity.this, "Authentication failed.",
+                         //           Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
                     }
                 });
+        // [END create_user_with_email]
+    }
+    private void updateUI(FirebaseUser user) {
 
+    }
     }
 
 
-}
--- app/src/main/java/osp/smgonggu/myapplication/LoginActivity.java
+++ app/src/main/java/osp/smgonggu/myapplication/LoginActivity.java
@@ -18,6 +18,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -43,16 +44,11 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private DatabaseReference mDatabase;// 파이어베이스 연동할 데이터베이스 리퍼런스 함수
-
     // 로그에 사용할 TAG 변수 선언
     final private String TAG = getClass().getSimpleName();
 
     //구글로그인을 하기 위한 함수
     private FirebaseAuth mAuth = null;
-    private GoogleSignInClient mGoogleSignInClient;
-    private static final int RC_SIGN_IN = 9001;
-    private SignInButton signInButton;
 
     // 사용할 컴포넌트 선언
     EditText userid_et, passwd_et;
@@ -63,7 +59,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        mDatabase = FirebaseDatabase.getInstance().getReference(); //
+        mAuth =  FirebaseAuth.getInstance();
 // 사용할 컴포넌트 초기화
         userid_et = findViewById(R.id.userid_et);
         passwd_et = findViewById(R.id.passwd_et);
@@ -75,8 +71,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 // 로그인 함수
-                LoginTask loginTask = new LoginTask();
-                loginTask.execute(userid_et.getText().toString(), passwd_et.getText().toString());
+               signIn(userid_et.getText().toString(),passwd_et.getText().toString());
             }
         });
 
@@ -91,129 +86,24 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    class LoginTask extends AsyncTask<String, Void, String> {
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-            Log.d(TAG, "onPreExecute");
-        }
-
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            Log.d(TAG, "onPostExecute, " + result);
-
-            if(result.equals("success")){
-// 결과값이 success 이면
-// 토스트 메시지를 뿌리고
-// userid 값을 가지고 ListActivity 로 이동
-                Toast.makeText(LoginActivity.this, "로그인 되었습니다.", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(LoginActivity.this, ListActivity.class);
-                intent.putExtra("userid", userid_et.getText().toString());
-                startActivity(intent);
-            }else
-            {
-                Toast.makeText(LoginActivity.this, result, Toast.LENGTH_SHORT).show();
-            }
-        }
-
-        private void writeNewUser(String userId, String name ,String password) {
-
-            User user = new User(name);
-
-            mDatabase.child("users").child(userId).setValue(user)
-                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+        private void signIn(String email, String password) {
+            // [START sign_in_with_email]
+            mAuth.signInWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
                         @Override
-                        public void onSuccess(Void aVoid) {
-                            // Write was successful!
-                            Toast.makeText(LoginActivity.this, "저장을 완료했습니다.", Toast.LENGTH_SHORT).show();
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            // Write failed
-                            Toast.makeText(LoginActivity.this, "저장을 실패했습니다.", Toast.LENGTH_SHORT).show();
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(LoginActivity.this, ListActivity.class);
+                                startActivity(intent);
+
+                            } else {
+                                Toast.makeText(LoginActivity.this, "로그인 오류", Toast.LENGTH_SHORT).show();
+                            }
                         }
                     });
-
         }
-        private void readUser(){
-            mDatabase.child("users").child("1").addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    // Get Post object and use the values to update the UI
-                    if(dataSnapshot.getValue(User.class) != null){
-                        User post = dataSnapshot.getValue(User.class);
-                        Log.w("FireBaseData", "getData" + post.toString());
-                    } else {
-                        Toast.makeText(LoginActivity.this, "데이터 없음...", Toast.LENGTH_SHORT).show();
-                    }
-                }
 
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-                    // Getting Post failed, log a message
-                    Log.w("FireBaseData", "loadPost:onCancelled", databaseError.toException());
-                }
-            });
-        }
-        @Override
-        protected String doInBackground(String... params) {
-
-            String userid = params[0];
-            String passwd = params[1];
-
-            String server_url = "https://sunmoongonggu-default-rtdb.firebaseio.com/\n";
-
-
-            URL url;
-            String response = "";
-            try {
-                url = new URL(server_url);
-
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(15000);
-                conn.setConnectTimeout(15000);
-                conn.setRequestMethod("POST");
-                conn.setDoInput(true);
-                conn.setDoOutput(true);
-                Uri.Builder builder = new Uri.Builder()
-                        .appendQueryParameter("userid", userid)
-                        .appendQueryParameter("passwd", passwd);
-                String query = builder.build().getEncodedQuery();
-
-                OutputStream os = conn.getOutputStream();
-                BufferedWriter writer = new BufferedWriter(
-                        new OutputStreamWriter(os, "UTF-8"));
-                writer.write(query);
-                writer.flush();
-                writer.close();
-                os.close();
-
-                conn.connect();
-                int responseCode=conn.getResponseCode();
-
-                if (responseCode == HttpsURLConnection.HTTP_OK) {
-                    String line;
-                    BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                    while ((line=br.readLine()) != null) {
-                        response+=line;
-                    }
-                }
-                else {
-                    response="";
-
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            return response;
-        }
-    }
+                        private void updateUI(FirebaseUser user) {};
 
 }
