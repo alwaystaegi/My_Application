6f16e3404696e2e9b122cea5b2ace2021ccdd14e
alwaystaegi <ffff4565@sunmoon.ac.kr>
2022-06-14 14:25:37 +0900

    현재 결과: 로그인,회원가입,게시글쓰기/읽기 구현, 댓글쓰기 오류

---
+++ app/src/main/java/osp/smgonggu/myapplication/Comment.java
@@ -0,0 +1,15 @@
+package osp.smgonggu.myapplication;
+
+public class Comment {
+    String user;
+    String comment;
+    String Boardindex;
+    String commentindex;
+ Comment(String user, String comment,String commentindex,String Boardindex){
+       this.user=user;
+       this.comment=comment;
+       this.commentindex=commentindex;
+       this.Boardindex=Boardindex;
+   }
+
+}
